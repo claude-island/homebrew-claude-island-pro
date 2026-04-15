5f71629a71bb7beb5a0aeb5322d7d88adf0c623f
lele <619923727@qq.com>
2026-04-15 20:01:37 +0800

    Update Claude Island to 1.0.0

--- Casks/claude-island.rb
+++ Casks/claude-island.rb
@@ -1,6 +1,6 @@
 cask "claude-island" do
   version "1.0.0"
-  sha256 "f13b0a6cd3cf2a929085b85ed261648bd0766425867a25b6b513fce2bc3a8529"
+  sha256 "1607c4fb4b6f479f5e89c3ccfd4609dd9a073ae75bbb3862e7f36615260d2ace"
 
   url "https://github.com/claude-island/ClaudeIsland/releases/download/v#{version}/ClaudeIsland.dmg"
   name "Claude Island"
